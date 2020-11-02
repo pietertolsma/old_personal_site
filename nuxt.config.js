2e70e387a2dae7dbae6b47675ce73d844a59a6ac
pieter <epieter.tolsma@gmail.com>
2020-11-02 16:52:06 +0100

    t

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
   target: 'static',
 
   router : {
-    base : '/pietertolsma.com/'
+    base : '/pietertolsma/'
   },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
