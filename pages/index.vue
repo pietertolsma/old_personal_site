d0190d91e990e72c30e8f77f21db14b928638894
pieter <epieter.tolsma@gmail.com>
2020-11-02 16:46:09 +0100

    init

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row justify="center" align="center">
+    PieterTolsma.com!
+  </v-row>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo,
+  },
+}
+</script>
